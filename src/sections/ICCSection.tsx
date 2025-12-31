da6f7e908314ce0020c467af28978ae5e3ee80e0
LesterOsana18 <lesterfosana@iskolarngbayan.pup.edu.ph>
2025-12-31 21:30:10 +0800

    feat: replace inline arrow button with ArrowButton component for cleaner code

--- src/sections/ICCSection.tsx
+++ src/sections/ICCSection.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { iccCase } from "../data/iccCase";
+import ArrowButton from "../components/ArrowButton";
 
-const ICCSection = () => {
+const ICCSection: React.FC = () => {
   const handleScroll = () => {
     document
       .getElementById("call-to-action-section")
@@ -99,32 +100,7 @@ const ICCSection = () => {
           </div>
         </div>
       </div>
-
-      {/* Arrow Down Button */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-        <button
-          onClick={handleScroll}
-          aria-label="Continue Reading"
-          className="
-            p-2 rounded-full bg-red-600 hover:bg-red-700 hover:scale-110 transition-all duration-300
-          "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </button>
-      </div>
+      <ArrowButton handleScroll={handleScroll} />
     </section>
   );
 };
